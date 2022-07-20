b8f58cd4ec43b1037644b368137d655a4b627db8
Stoyann <stoyann.openclassrooms@gmail.com>
2022-07-20 22:30:48 +1100

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,33 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
+
+
 // ==== Variables & initialisation
 const PORT = process.env.PORT || 5000
 const app = express()
-
+const connectDB = require('./config/db')
 // ==== Middlewares
 const { errorHandler } = require('./middleware/errorMiddleware')
 
-app.use(express.urlencoded({ extended: false }))
+
+
+//connect to database
+connectDB()
+
+
 
 // ==== Routes
+app.use(express.urlencoded({ extended: false }))
 app.use(errorHandler)
 //Root URL
 app.get('/', (req, res) => {
     res.status(200).send({ message: `Bienvenue sur l'api de krysto` })
 })
+
 //User routes
 app.use('/api/users', require('./routes/userRoutes'))
 
 
 // ==== Start server
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`========= Server started on port  http://localhost:${PORT} ========`.white.underline.bold.bgGreen))
